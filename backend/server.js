a292e9fff66f9b6809e62d09cb06256684f0781a
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2024-12-16 13:11:19 +0530

    server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
+const gameRoutes = require("./routes/games");
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(cors());
 
 // Routes
 app.use("/auth", authRoutes);
+app.use("/games", gameRoutes);  // This will handle routes like GET /games
 
 // Start server
 app.listen(3001, () => console.log("Server running on port 3001"));
