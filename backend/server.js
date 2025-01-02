a84e613bf315666a1b471cefe5af44eeed0996e4
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2025-01-02 20:57:31 +0530

    modify server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,39 @@
-const express = require("express");
-const cors = require("cors");
-const authRoutes = require("./routes/auth");
-const gameRoutes = require("./routes/games");
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors'); 
+const routes = require('./routes');
+const db = require('./db/db');
 
 const app = express();
+const server = http.createServer(app);
+
+// Enable CORS
+app.use(cors({
+  origin: 'http://localhost:3001',
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
+
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3001', 
+    methods: ['GET', 'POST'],
+  },
+});
+
 app.use(express.json());
-app.use(cors());
+app.use('/api', routes);
 
-// Routes
-app.use("/auth", authRoutes);
-app.use("/games", gameRoutes); 
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('move', (data) => {
+    io.emit('updateGame', data); 
+  });
 
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
 
-app.listen(3001, () => console.log("Server running on port 3001"));
+server.listen(3000, () => console.log('Server running on port 3000'));
