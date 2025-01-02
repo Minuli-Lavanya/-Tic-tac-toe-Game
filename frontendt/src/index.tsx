339996e1b39bbc2fafe09f79b1b8317e8d3997d4
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2025-01-02 21:09:22 +0530

    add index file

--- frontendt/src/index.tsx
+++ frontendt/src/index.tsx
@@ -1,15 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.tsx';
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+
+ 
+
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./App.css";
+
+// const rootElement = document.getElementById("root")!;
+// const root = ReactDOM.createRoot(rootElement);
+
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
 
