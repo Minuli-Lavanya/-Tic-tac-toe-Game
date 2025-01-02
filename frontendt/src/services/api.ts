c8961417d9da3aa5f1a0b291b6254db442015565
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2025-01-02 22:08:09 +0530

    add api ts

--- frontendt/src/services/api.ts
+++ frontendt/src/services/api.ts
@@ -1,20 +1,33 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = "http://localhost:3001/auth";
-
-const api = axios.create({
-    baseURL: API_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+const API = axios.create({
+  baseURL: 'http://localhost:3000/api',
 });
 
-// Register user
-export const registerUser = (username: string, password: string) =>
-    api.post("/register", { username, password });
+export const registerUser = async (username: string, password: string) => {
+  return await API.post('/auth/register', { username, password });
+};
 
-// Login user
-export const loginUser = (username: string, password: string) =>
-    api.post("/login", { username, password });
+export const loginUser = async (username: string, password: string) => {
+  return await API.post('/auth/login', { username, password });
+};
 
-export default api;
+export const createGame = async (token: string) => {
+  return await API.post(
+    '/game',
+    {},
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+};
+
+export const makeMove = async (gameId: number, move: number, token: string) => {
+  return await API.post(
+    '/game/move',
+    { gameId, move },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+};
